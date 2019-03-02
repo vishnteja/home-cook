7d863767af5e6bcc15b2c082c13cd59fe85c1e61
N Vishnuteja Reddy <ashanthrokser22@gmail.com>
2019-03-02 16:09:29 +0530

    Setup

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" mb-0 h1>
+          Navbar
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
