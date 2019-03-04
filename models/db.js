ac7660359c994c9419030e5ce51aa602d41d0e86
Nikhil Nair <niks.nair@gmail.com>
2019-03-04 19:32:20 +0530

    connected to MongoDB Atlas

--- models/db.js
+++ models/db.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 // Allow Promises
 mongoose.Promise = global.Promise;
 // Connection
-mongoose.connect("mongodb://localhost:27017/homeDB", {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  "mongodb+srv://hkTester:pwd2318@test-k51n5.mongodb.net/test?retryWrites=true",
+  {
+    // mongoose.connect("mongodb://localhost:27017/homeDB", {
+    useNewUrlParser: true
+  }
+);
 // Validation
 mongoose.connection
   .once("open", () => console.log("Connected to the database!"))
