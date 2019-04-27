db6114c35cf24296935e7a17fa64f30c94a7cf30
Nikhil Nair <niks.nair@gmail.com>
2019-04-27 23:08:12 +0530

    pre-deploy 0.3

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const customer = require("./routes/customer");
 
@@ -29,6 +30,15 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api", customer);
+
+//for production - serving static assets
+if (process.env.NODE_ENV == "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
